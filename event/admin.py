33dbaf607123f0b6b03e7ff4902f14a8046ffee2
Deger Ahmet <adeger@protonmail.com>
2022-02-23 17:23:45 -0500

    admin registers okay

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Event,TicketPrice,Ticket
 # Register your models here.
+
+admin.site.register(Event)
+admin.site.register(TicketPrice)
+admin.site.register(Ticket)
--- place/admin.py
+++ place/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Seat,SeatingPlan,Place
 
 # Register your models here.
+
+
+admin.site.register(SeatingPlan)
+admin.site.register(Seat)
+admin.site.register(Place)
