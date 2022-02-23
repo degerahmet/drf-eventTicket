7425d8794019c2ae50f3766b6c7315c1a89ba797
Deger Ahmet <adeger@protonmail.com>
2022-02-23 16:22:58 -0500

    rest_framework added

--- drf_eventTicket/settings.py
+++ drf_eventTicket/settings.py
@@ -40,7 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+
+    'drf_yasg',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
